59d104fe1752135ab45aa24706338965a29f876e
Konrad Bakowski <konri0908@gmail.com>
2023-11-03 10:08:39 +0100

    template

---
+++ template.s
@@ -0,0 +1,12 @@
+.section .text
+.globl _start
+
+_start:
+    
+    mov $5, %ECX
+    mov $-5, %EDX
+
+    # etykieta po wprowadzenia danych wejściowych
+__begin:
+
+    # zerować EDX czy inne rejestry jesli potrzeba w tej części
